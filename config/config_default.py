c34aa4ba77cc31f8c4ce2ac9ba39bbf73292472b
cheungbh <534660436@qq.com>
2020-07-10 18:10:57 +0800

    cfg default

--- config/config_default.py
+++ config/config_default.py
@@ -1,8 +1,15 @@
+device = "cuda:0"
+
+black_yolo_cfg = "weights/yolo/0710/black/yolov3-spp-1cls.cfg"
+black_yolo_weights = 'weights/yolo/0710/black/150_416_best.weights'
+gray_yolo_cfg = "weights/yolo/0710/gray/yolov3-spp-1cls.cfg"
+gray_yolo_weights = 'weights/yolo/0710/gray/135_608_best.weights'
+
 # For yolo
-confidence = 0.5
+confidence = 0.05
 num_classes = 80
 nms_thresh = 0.33
-input_size = 608
+input_size = 416
 
 # For pose estimation
 input_height = 320
@@ -16,29 +23,9 @@ pose_batch = 80
 water_top = 40
 
 # For detection
-video_num = 48
-video_path = "/media/hkuit164/7024-A194/0507 New"
-frame_size = (720, 560)
-rgb_yolo_cfg = "config/yolo_cfg/yolov3-spp-1cls.cfg"
-rgb_yolo_weights = '/media/hkuit164/WD20EJRX/spp/150_16_dark_yes_608_multi/last.weights'
-black_yolo_cfg = "config/yolo_cfg/yolov3-spp-1cls.cfg"
-black_yolo_weights = '/media/hkuit164/WD20EJRX/black_we/200_16_dark_yes_416_multi_150/best.weights'
-# black_yolo_weights = 'models/yolo/best_converted.weights'
-gray_yolo_cfg = "config/yolo_cfg/yolov3-spp-1cls.cfg"
-gray_yolo_weights = '/media/hkuit164/WD20EJRX/multi/gray_200_16_dark_yes_608_multi_135/best.weights'
-# gray_yolo_weights ='/media/hkuit164/WD20EJRX/multi/200_16_dark_yes_416_multi_200_cls10/best.weights'
-# gray_yolo_weights ='/media/hkuit164/WD20EJRX/11/200_16_dark_yes_416_multi_200_arc/last.weights'#best
-# gray_yolo_weights = 'models/yolo/spp/200_16_dark_yes_608_multi/last.weights'#best
-horizontal = 4
-vertical = 3
-device = 'cuda:0'
 
-water_top = 40
-
-# For detection
-video_num = 5
-video_path = "video/origin/{}.mp4".format(video_num)
-frame_size = (540, 360)
+video_path = "video/0710/0048.mp4"
+frame_size = (720, 540)
 
 black_box_threshold = 0.3
 gray_box_threshold = 0.3
